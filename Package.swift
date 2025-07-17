7697cfc7b9f8643934713b7a3328948f19bbd35a
sookim-1 <scstnghks@gmail.com>
2025-07-17 18:53:26 +0900

    chore : rename k-sdui-ios

--- Package.swift
+++ Package.swift
@@ -4,18 +4,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "sdui-ios",
+    name: "k-sdui-ios",
     platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "sdui-ios",
-            targets: ["sdui-ios"]),
+            name: "k-sdui-ios",
+            targets: ["k-sdui-ios"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "sdui-ios")
+            name: "k-sdui-ios")
     ]
 )
